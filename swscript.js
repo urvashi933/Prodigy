7173289d9e03bd766af4c14895d5eed503f17503
urvashi933 <srivastavaurvashi933@gmail.com>
2024-09-10 11:58:53 +0530

    Create swscript.js

---
+++ swscript.js
@@ -0,0 +1,72 @@
+
+let seconds = 00;
+let tens = 00;
+let mins = 00;
+let getSeconds = document.querySelector('.seconds');
+let getTens = document.querySelector('.tens');
+let getMins = document.querySelector('.mins');
+let btnStart = document.querySelector('.btn-start');
+let btnStop = document.querySelector('.btn-stop');
+let btnReset = document.querySelector('.btn-reset');
+let btnLap = document.querySelector('.btn-lap');
+
+let interval=null;
+let lapNow = null;
+
+btnStart.addEventListener('click', () => {
+    clearInterval(interval);
+    interval = setInterval(startTimer, 10);
+})
+btnLap.addEventListener('click', lap)
+
+btnStop.addEventListener('click', () => {
+    clearInterval(interval);
+
+})
+btnReset.addEventListener('click', () => {
+    clearInterval(interval);
+    tens = '00';
+    seconds = '00';
+    mins = '00';
+    getSeconds.innerHTML = seconds;
+    getTens.innerHTML = tens;
+    getMins.innerHTML = mins;
+})
+
+function startTimer(){
+    tens++;
+    if(tens <= 9){
+        getTens.innerHTML = '0' + tens;
+    }
+    if(tens > 9){
+        getTens.innerHTML = tens;
+    }
+    if(tens > 99){
+        seconds++;
+        getSeconds.innerHTML = '0' + seconds;
+        tens = 0;
+        getTens.innerHTML = '0' + 0;
+    }
+    if(seconds > 9){
+        getSeconds.innerHTML = seconds;
+    }
+    if(seconds > 59){
+        mins++;
+        getMins.innerHTML = '0' + mins;
+        seconds = 0;
+        getSeconds.innerHTML = '0' + 0;
+    }
+    if(mins > 9){
+        getSeconds.innerHTML = mins;
+    }
+}
+function stop() {
+        this.running = false;
+        this.time = null;
+    }
+
+function lap() {
+  lapNow =  mins + " : " + seconds + " : " + tens+'<br>' ;
+  laps = document.getElementById('lapRecord').innerHTML + lapNow;
+document.getElementById('lapRecord').innerHTML= laps;
+}
